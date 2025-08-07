25ea16b3ab5883c1e77da9be38e19c13a8e5a0fa
atimaby28 <atimaby28@gmail.com>
2025-08-07 11:42:08 +0900

    Update : Repository

--- src/main/java/org/example/springjpa/book/repository/BookRepository.java
+++ src/main/java/org/example/springjpa/book/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package org.example.springjpa.book.repository;
 
-public interface BookRepository {
+import org.example.springjpa.book.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
 }
